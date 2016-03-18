a9bad17a8894f0884766b9e2360f68bb1b8c232c
cgpllx <cgpllx1@qq.com>
2016-03-18 14:05:10 +0800

    fix 多选时候的效果

---
+++ library/src/main/java/cc/easyandroid/listfiltermenu/core/SimpleEasyItem.java
@@ -0,0 +1,35 @@
+package cc.easyandroid.listfiltermenu.core;
+
+import java.util.List;
+
+/**
+ * simple IEasyItem
+ */
+public class SimpleEasyItem implements IEasyItem {
+    private int childSelectPosion;
+
+    @Override
+    public List<? extends IEasyItem> getChildItems() {
+        return null;
+    }
+
+    @Override
+    public int getChildSelectPosion() {
+        return childSelectPosion;
+    }
+
+    @Override
+    public void setChildSelectPosion(int posion) {
+        this.childSelectPosion = posion;
+    }
+
+    @Override
+    public CharSequence getDisplayName() {
+        return null;
+    }
+
+    @Override
+    public CharSequence getEasyId() {
+        return null;
+    }
+}
